a47f26e316ae0ddbcf535d5f04b83a359cf61e5e
Girish21 <girishkumarv21@gmail.com>
2021-07-29 23:40:02 +0530

    better defaults

--- shader/test/fragment.frag
+++ shader/test/fragment.frag
@@ -1,3 +1,8 @@
+uniform float uTime;
+uniform vec2 uResolution;
+
 void main() {
-  gl_FragColor = vec4(1.);
+  vec2 uv = (gl_FragCoord.xy - uResolution) / uResolution.yy;
+
+  gl_FragColor = vec4(uv, 1., 1.);
 }
--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
 const material = new THREE.ShaderMaterial({
   vertexShader: VertexShader,
   fragmentShader: FragmentShader,
+  uniforms: {
+    uTime: { value: 0 },
+    uResolution: { value: new THREE.Vector2(0.0, 0.0) },
+  },
 })
 
 const mesh = new THREE.Mesh(geometry, material)
@@ -25,6 +29,11 @@ const size = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
+
+material.uniforms.uResolution.value = new THREE.Vector2(
+  window.innerWidth,
+  window.innerHeight
+)
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -49,6 +58,11 @@ window.addEventListener('resize', () => {
   size.width = window.innerWidth
   size.height = window.innerHeight
 
+  material.uniforms.uResolution.value = new THREE.Vector2(
+    window.innerWidth,
+    window.innerHeight
+  )
+
   camera.aspect = size.width / size.height
   camera.updateProjectionMatrix()
 
@@ -58,6 +72,8 @@ window.addEventListener('resize', () => {
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  material.uniforms.uTime.value = elapsedTime
 
   mesh.rotation.set(0, elapsedTime / 5.0, 0)
 
